32b1e61782038d72ea43363b0ed2c50e5c20c704
Arusham Gaur <arushamgour771@gmail.com>
2023-07-10 13:03:10 +0530

    intro to js

---
+++ SECTION 1/index.js
@@ -0,0 +1,25 @@
+console.log('Hello JavaScript');
+
+num=247;
+
+console.log(num);
+
+myname="Arusham";
+
+console.log(myname);
+
+isLoggedin=false;
+
+console.log(typeof isLoggedin);
+console.log(typeof num);
+
+if(num%2==0){
+    console.log('even');
+}else{
+    console.log('odd');
+    a=23456;
+};
+
+console.log(a);
+
+console.log('234'==234)
