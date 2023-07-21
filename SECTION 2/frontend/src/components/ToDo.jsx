b27a9224a01954db830dcc8c706f67f785228c10
Arusham Gaur <arushamgour771@gmail.com>
2023-07-21 14:16:06 +0530

    todo

--- SECTION 2/frontend/src/components/ToDo.jsx
+++ SECTION 2/frontend/src/components/ToDo.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ToDo = () => {
+
+    const [todoList, setTodoList] = useState([]);
     
     const addtodo = (e) => {
-        console.log(e.code);
+        //console.log(e.code);
         if(e.code === 'Enter'){
             console.log('task added');
+            let task = e.target.value;
+            if(!task.trim()) return;
+            e.target.value = '';
+
+            setTodoList([...todoList, { task: task, completed: false }]);
+            console.log(todoList);
         }
+    };
+
+    const completeTodo = (index) => {
+        const temp = todoList;
+        temp[index].completed = true;
+        setTodoList([...temp]);
+    }
+
+    const deleteTodo = (index) => {
+        const temp = todoList;
+        temp.splice(index, 1);
+        setTodoList([...temp]);
     }
 
   return (
@@ -17,7 +37,17 @@ const ToDo = () => {
                 <div className="card-header">
                     <input type="text" className='form-control' onKeyDown={addtodo}/>
                 </div>
-                <div className="card-body"></div>
+                <div className="card-body">
+                    <ul className='list-group'>
+                    {todoList.map((todo, index) => (
+                        <li className='list-group-item' key={index}> 
+                            <h3>{todo.task}</h3>
+                            <input type="checkbox" />
+                            <button className={`ms-3 btn ${todo.completed ? 'btn-success' : 'btn-warning'}`} onClick={() => { completeTodo(index) }}>{todo.completed ? "Completed" : " Pending"}</button>
+                            <button className='ms-3 btn btn-danger'  onClick={() => { deleteTodo(index) }}>Delete</button>
+                        </li>))}
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
