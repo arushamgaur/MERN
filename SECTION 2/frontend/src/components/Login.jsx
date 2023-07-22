7242b0438ce86043573308862213becfef4e3e2b
Arusham Gaur <arushamgour771@gmail.com>
2023-07-22 14:00:51 +0530

    formik

--- SECTION 2/frontend/src/components/Login.jsx
+++ SECTION 2/frontend/src/components/Login.jsx
@@ -1,5 +1,10 @@
 import { useFormik } from "formik"
 import React from 'react'
+import * as Yup from 'yup';
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+});
 
 const Login = () => {
 
@@ -13,7 +18,9 @@ const Login = () => {
     onSubmit: (values) => {
       console.log(values);
       //submit value to the backend
-    }
+    },
+
+    validationSchema: loginSchema
   });
 
   return (
@@ -24,6 +31,7 @@ const Login = () => {
           <h2 className="text-center my-5">LOGIN FORM</h2>
           <form onSubmit={loginform.handleSubmit}>
             <label htmlFor="">Email</label>
+            <p>{loginform.errors.email}</p>
             <input className="form-control mb-4 rounded-3" type="email" name="email" onChange={loginform.handleChange} value={loginform.values.email} />
             <label htmlFor="">Password</label>
             <input className="form-control mb-4 rounded-3" type="password" name="password" onChange={loginform.handleChange} value={loginform.values.password} />
